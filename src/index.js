8084370c82df7c7f2152f4723cfe6859f51dbddc
Rick Umali <rickumali@gmail.com>
2023-07-25 20:47:27 -0400

    Removes reportWebVitals()
    I'm not using this feature

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 let notesJson = "[]";
 if (typeof window !== 'undefined') { // Check if we're running in the browser.
@@ -15,8 +14,3 @@ root.render(
     <App notesFromStorage={notesJson} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
